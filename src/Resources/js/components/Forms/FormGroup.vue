b8099718d4ada84561efe8d6f50976f6deafc761
Marek Gogol <9mike66@gmail.com>
2020-06-19 20:32:48 +0200

    fixed tab padding

--- src/Resources/js/components/Forms/FormGroup.vue
+++ src/Resources/js/components/Forms/FormGroup.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-show="isGroupVisible(group)" class="fields-group fields-group__item" :group-id="group.id" :class="getGroupClass(group)" :data-fields="visibleFields.length">
-        <div :class="{ card : canShowGroupName(group), 'fields-group__wrapper' : visibleFields.length > 0 && !hasTabs(group.fields) }">
+        <div :class="{ card : canShowGroupName(group), 'fields-group__wrapper' : visibleFields.length > 0 && !hasTabs(group.fields), 'fields-tabs__wrapper' : hasTabs(group.fields) }">
             <h4 class="card-header" v-if="canShowGroupName(group)">
                 <i v-if="group.icon" class="card-header--icon-left" :class="['fa', faMigrator(group.icon)]"></i>
                 <span v-html="group.name"></span>
@@ -8,7 +8,7 @@
 
             <div :class="{ 'card-body' : canShowGroupName(group) }">
                 <div class="row">
-                    <div v-if="hasTabs(group.fields)" class="col-lg-12">
+                    <div v-if="hasTabs(group.fields)" class="col-lg-12 tab--parent">
                         <form-tabs-builder
                             :tabs="tabsFields"
                             :model="model"
