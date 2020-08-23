0271e2702529c4bddd39c176d8bb53f5621a5dd1
Marek Gogol <9mike66@gmail.com>
2020-08-23 11:34:56 +0200

    fix

--- src/Resources/admin/plugins/ckeditor/config.js
+++ src/Resources/admin/plugins/ckeditor/config.js
@@ -2,8 +2,6 @@
  * @license Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
-CKEDITOR.timestamp = 'test';
-
 CKEDITOR.editorConfig = function( config ) {
     var lang = $('html').attr('lang')||'sk';
 
