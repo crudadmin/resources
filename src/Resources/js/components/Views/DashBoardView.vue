191ea69fff539f518cfc3122051cbaefc07cf50a
Marek Gogoľ <info@marekgogol.sk>
2021-07-10 12:29:37 +0200

    added vuejs dashboard support

--- src/Resources/js/components/Views/DashBoardView.vue
+++ src/Resources/js/components/Views/DashBoardView.vue
@@ -9,7 +9,7 @@
 
         <!-- Main content -->
         <section class="crudadmin-wrapper" v-show="user">
-            <div class="box" v-if="!layout">
+            <div class="box" v-if="!isCustomLayout">
                 <div class="box-body">
                     <div class="box-body-wrapper">
                         <h2 v-if="user">{{ trans('welcome') }} {{ user.username }}</h2>
@@ -17,7 +17,10 @@
                 </div>
             </div>
 
-            <div v-html="layout"></div>
+            <div v-else>
+                <div v-if="htmlLayout" v-html="htmlLayout"></div>
+                <component v-if="dashboard.vue" :is="dashboard.vue"></component>
+            </div>
         </section>
         <!-- /.content -->
     </div>
@@ -28,19 +31,29 @@
         props : ['langid'],
 
         mounted(){
-
+            console.log(this.dashboard);
         },
 
         computed: {
             user(){
                 return this.$root.user;
             },
-            layout(){
+            dashboard(){
+                return this.$root.dashboard;
+            },
+            isCustomLayout(){
+                return this.dashboard;
+            },
+            htmlLayout(){
+                if ( !(this.dashboard && this.dashboard.html) ) {
+                    return;
+                }
+
                 this.$nextTick(() => {
-                    this.$root.runInlineScripts(this.$root.dashboard)
+                    this.$root.runInlineScripts(this.dashboard.html)
                 });
 
-                return this.$root.dashboard;
+                return this.dashboard.html;
             },
         },
 
