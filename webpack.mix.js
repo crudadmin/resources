95f0dd77a684870c2b30d5626653f3b8d5cf30c2
Marek Gogoľ <info@marekgogol.sk>
2024-06-17 20:17:20 +0200

    node scripts removed

--- webpack.mix.js
+++
@@ -1,9 +0,0 @@
-let mix = require('laravel-mix'),
-    section = process.env.npm_config_section || 'admin';
-
-if (['admin', 'web'].includes(section)) {
-    require(`${__dirname}/webpack.${section}.mix.js`);
-} else {
-    console.log('\x1b[41m%s\x1b[0m', 'Provide correct --section argument to build command: admin, web');
-    throw new Error('Provide correct --section argument to build command!');
-}
